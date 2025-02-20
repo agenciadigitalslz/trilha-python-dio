7c85684b8fb5627d9e68dd2ee36f6bb80012908a
agenciadigitalslz <aslfama@gmail.com>
2025-02-20 14:08:02 -0300

    testandos os objetos

--- 02 - Programação Orientada a Objetos/02 - Classes e Objetos/01_desafio_bicicletaria.py
+++ 02 - Programação Orientada a Objetos/02 - Classes e Objetos/01_desafio_bicicletaria.py
@@ -15,16 +15,29 @@ class Bicicleta:
     def correr(self):
         print("Vrummmmm...")
 
+    def trocar(self):
+        print("Trocando a bicicleta!")
+
+    def trocada(self):
+        print("Bicicleta trocada!")
+    
+    def agora(self):
+        print("Estou na biscicleta!")
+
     def __str__(self):
         return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
 
-
-b1 = Bicicleta("vermelha", "caloi", 2022, 600)
+b1 = Bicicleta("Vermelha", "Caloi", 2022, 600)
 b1.buzinar()
 b1.correr()
 b1.parar()
 print(b1.cor, b1.modelo, b1.ano, b1.valor)
 
-b2 = Bicicleta("verde", "monark", 2000, 189)
-print(b2)
+b1.trocar()
+b1.trocada()
+b1.agora()
+
+b2 = Bicicleta("Verde", "Monark", 2000, 189)
+print(b2.cor, b2.modelo, b2.ano, b2.valor)
+# print(b2)
 b2.correr()
